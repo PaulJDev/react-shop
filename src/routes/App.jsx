f600351059daf02f7169e80f4860b14f8d45bb44
Paul <pauljavaloyesdev@gmail.com>
2022-03-12 23:16:41 +0100

    first commit

---
+++ src/routes/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Layout from '../containers/Layout.jsx';
+import Login from '../containers/Login.jsx';
+import RecoveryPassword from '../containers/RecoveryPassword.jsx';
+import Home from '../pages/Home.jsx'
+import NotFound from '../pages/NotFound.jsx'
+import '../styles/global.css'
+
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Layout>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/recovery-password" element={<RecoveryPassword />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </Layout>
+        </BrowserRouter> 
+    );
+}
+
+export default App;
