b504f44bb2d9263a10afc739182a843e85ca45ca
Paul <pauljavaloyesdev@gmail.com>
2022-03-13 10:01:18 +0100

    Big commit

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Layout from '../containers/Layout.jsx';
-import Login from '../containers/Login.jsx';
-import RecoveryPassword from '../containers/RecoveryPassword.jsx';
+import Login from '../pages/Login.jsx';
+import SendEmail from '../pages/SendEmail.jsx';
+import NewPassword from '../pages/NewPassword.jsx';
+import MyAccount from '../pages/MyAccount.jsx';
+import CreateAccount from '../pages/CreateAccount.jsx';
+import Checkout from '../pages/Checkout.jsx';
+import Orders from '../pages/Orders.jsx';
+import RecoveryPassword from '../pages/RecoveryPassword.jsx';
 import Home from '../pages/Home.jsx'
 import NotFound from '../pages/NotFound.jsx'
 import '../styles/global.css'
@@ -15,6 +21,12 @@ const App = () => {
                     <Route path="/" element={<Home />} />
                     <Route path="/login" element={<Login />} />
                     <Route path="/recovery-password" element={<RecoveryPassword />} />
+                    <Route path="/send-email" element={<SendEmail />} />
+					<Route path="/new-password" element={<NewPassword />} />
+					<Route path="/account" element={<MyAccount />} />
+					<Route path="/signup" element={<CreateAccount />} />
+					<Route path="/checkout" element={<Checkout />} />
+					<Route path="/orders" element={<Orders />} />
                     <Route path="*" element={<NotFound />} />
                 </Routes>
             </Layout>
